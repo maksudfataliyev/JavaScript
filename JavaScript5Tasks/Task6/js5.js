7c627926db6a563145151830dc4660f682e7a4bf
maksudfataliyev <143101499+maksudfataliyev@users.noreply.github.com>
2025-05-15 21:17:14 +0400

    Update js5.js

--- JavaScript5Tasks/Task6/js5.js
+++ JavaScript5Tasks/Task6/js5.js
@@ -3,6 +3,7 @@ let button2 = document.getElementById("button2");
 let tip1 = document.getElementById("tip1");
 let tip2 = document.getElementById("tip2");
 
+
 button1.addEventListener('mouseover',function(){
     tip1.style.display = "block";
 });
